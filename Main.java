2c27e9aa6f50ddf23d2e58c3c3f1089807ead880
artinsh03 <mohammadshamsy03@gmail.com>
2023-06-07 12:26:43 +0330

    Table ver 1.0

--- Main.java
+++ Main.java
@@ -14,10 +14,16 @@ public class Main{
 		double size = 0;
 		//Define DecimalFormat for formating size value
 		DecimalFormat df = new DecimalFormat("##.#");
+		//Print Header
+		String header = "|------------------------ " + path + " ----------------------|";
+		System.out.println("\u001B[36m"+header + "\u001B[0m");
 
 		byte index = 0;
+		int len = 0;
 		//Print files information
 		for(File f : files){
+			//Print Table border
+			System.out.print("\u001B[36m| \u001B[0m");
 			//Check if object is a file or folder
 			if(f.isFile()){
 				//Get file size (byte)
@@ -27,12 +33,31 @@ public class Main{
 					size /= 1024;
 					index++;
 				}
-				System.out.println(f.getName() + "  " + df.format(size) + " " + units[index]);
+				String temp = f.getName() + "  \u001B[33m" + df.format(size) + " " + units[index] + "\u001B[0m";
+				System.out.print(temp);
+				len = temp.length() - 9;
 			}else{
-				System.out.println("\u001B[35m" + f.getName() + "\u001B[0m");
+				System.out.print("\u001B[35m" + f.getName() + "\u001B[0m");
+				len = f.getName().length();
 			}
 			index = 0;
+			for(int i = 0 ; i < header.length() - len - 3 ; i++){
+				System.out.print(" ");
+			}
+			System.out.println("\u001B[36m|\u001B[0m");
+			drawLine(header.length());
 		}
 		
+	}
+	private static void drawLine(int length){
+		System.out.print("\u001B[36m");
+		for(int i = 0 ; i < length ; i++){
+			if(i == 0 || i == length - 1){
+				System.out.print("|");
+			}else{
+				System.out.print("-");
+			}
+		}
+		System.out.println("\u001B[0m");
 	}
 }
