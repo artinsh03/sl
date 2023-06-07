4938b7829f080b0c5a7bab7ccaf593ad9cad0211
artinsh03 <mohammadshamsy03@gmail.com>
2023-06-07 11:49:55 +0330

    Base

---
+++ Main.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.text.DecimalFormat;
+
+public class Main{
+	public static void main(String[] args){
+		//Define path
+		final String path = System.getProperty("user.dir");
+		//Define a array for memory units
+		final String[] units = new String[]{"Bytes" , "KB" , "MB" , "GB"};
+		//Get list of files
+		File dir = new File(path);
+		File[] files = dir.listFiles();
+		//Define a variable for size of files
+		double size = 0;
+		//Define DecimalFormat for formating size value
+		DecimalFormat df = new DecimalFormat("##.#");
+
+		byte index = 0;
+		//Print Header
+		System.out.println("\u001B[36m---------- " + path + " ----------\u001B[0m");
+		//Print files information
+		for(File f : files){
+			//Check if object is a file or folder
+			if(f.isFile()){
+				//Get file size (byte)
+				size = f.length();
+				//Convert units
+				while(size >= 1024){
+					size /= 1024;
+					index++;
+				}
+				System.out.println(f.getName() + "  " + df.format(size) + " " + units[index]);
+			}else{
+				System.out.println("\u001B[35m" + f.getName() + "\u001B[0m");
+			}
+			index = 0;
+		}
+		//Print Footer
+		System.out.print("\u001B[36m");
+		for(int i = 0 ; i < path.length() + 22 ; i++){
+			System.out.print("-");
+		}
+		System.out.println("\u001B[0m");
+	}
+}
---
+++ Makefile
@@ -0,0 +1,7 @@
+compile:
+	javac Main.java
+execute:
+	java Main
+clean:
+	rm *.class
+run:compile execute clean
